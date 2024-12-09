26b9f184a339071067555d4b4083419ee4e95c8e
AnanthaPushpaKaleeswaran <ananthapushpakaleeswaran@gmail.com>
2024-12-09 23:42:27 +0530

    correct

--- JenkinsTestWinUI/MainWindow.xaml.cs
+++ JenkinsTestWinUI/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace JenkinsTestWinUI
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked me";
+            myButton.Content = "Clicked me in a correct way";
         }
     }
 }
